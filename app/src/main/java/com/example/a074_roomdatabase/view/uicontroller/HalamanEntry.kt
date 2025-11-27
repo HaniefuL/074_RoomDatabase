1ebe26be01f656dc0a149a29b1bb50415afad12f
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-27 14:36:31 +0700

    menambah EntrySiswaScreen dengan ViewModel dan navigasi kembali

---
+++ app/src/main/java/com/example/a074_roomdatabase/view/uicontroller/HalamanEntry.kt
@@ -0,0 +1,12 @@
+package com.example.a074_roomdatabase.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) 

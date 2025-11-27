57bea5c19f05ef89e5b3f48fa2778ed4c258407a
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-27 14:37:45 +0700

    menambah EntrySiswaScreen dengan Scaffold, TopAppBar, dan aksi simpan

--- app/src/main/java/com/example/a074_roomdatabase/view/uicontroller/HalamanEntry.kt
+++ app/src/main/java/com/example/a074_roomdatabase/view/uicontroller/HalamanEntry.kt
@@ -9,4 +9,31 @@ fun EntrySiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) 
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
+            )
+        }) { innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}

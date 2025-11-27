e9e1182f8c3d82dbd1666e3d7dbb101129d6546a
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-27 14:46:54 +0700

    menambah SiswaApp dan HostNavigasi dengan konfigurasi NavHost

---
+++ app/src/main/java/com/example/a074_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,26 @@
+package com.example.a074_roomdatabase.view.uicontroller
+
+@Composable
+fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+
+    }
+}

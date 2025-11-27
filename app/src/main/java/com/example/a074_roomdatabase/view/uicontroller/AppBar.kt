18f4c4b10219af6a6963efc32afa9ce52239d294
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-27 14:42:48 +0700

    Menambahkan komponen CenterAlignedTopAppBar yang menampilkan judul halaman, opsi navigasi kembali dengan ikon ArrowBack, serta mendukung scrollBehavior.

---
+++ app/src/main/java/com/example/a074_roomdatabase/view/uicontroller/AppBar.kt
@@ -0,0 +1,25 @@
+package com.example.a074_roomdatabase.view.uicontroller
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        })
+}

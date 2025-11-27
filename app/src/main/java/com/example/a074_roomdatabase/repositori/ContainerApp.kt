cf977e64cd71a21a6b5b5cc3cbc44b02ef40380f
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-27 14:05:03 +0700

    Menambah ContainerApp dan ContainerDataApp untuk inisialisasi repositori siswa

---
+++ app/src/main/java/com/example/a074_roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,16 @@
+package com.example.a074_roomdatabase.repositori
+
+import android.content.Context
+import com.example.myroomsatu.room.DatabaseSiswa // Asumsi DatabaseSiswa ada di package room
+
+interface ContainerApp {
+    val repositoriSiswa: RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context) : ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa( // Pastikan class ini sudah Anda buat
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
+}

bb92dddc0200ab3b4ce2f0e474a2b43b0a697f7c
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-04 11:48:33 +0700

    menambahkan 3 override

--- app/src/main/java/com/example/a074_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a074_roomdatabase/repositori/RepositoriSiswa.kt
@@ -8,6 +8,12 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
 
     suspend fun insertSiswa(siswa: Siswa)
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun updateSiswa(siswa: Siswa)
+
+    suspend fun deleteSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -19,4 +25,9 @@ class OfflineRepositoriSiswa(
 
     override suspend fun insertSiswa(siswa: Siswa) =
         siswaDao.insert(siswa)
+    //Tambah 3 override
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.update(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
 }

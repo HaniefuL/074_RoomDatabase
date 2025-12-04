3934a61462d275d5d81b2a3cd5a5e269a5da2ec0
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-04 14:02:03 +0700

    implement data loading pada EditViewModel dengan SavedStateHandle dan UIStateSiswa

--- app/src/main/java/com/example/a074_roomdatabase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/a074_roomdatabase/viewmodel/EditViewModel.kt
@@ -1,6 +1,23 @@
 package com.example.a074_roomdatabase.viewmodel
 
-class EditViewModel {
+class EditViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoriSiswa: RepositoriSiswa
+): ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(
+        savedStateHandle[DestinasiDetailSiswa.itemIdArg]
+    )
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoriSiswa.getSiswaStream(idSiswa)
+                .filterNotNull()
+                .first()
+                .toUIStateSiswa(true)
+        }
+    }
 }

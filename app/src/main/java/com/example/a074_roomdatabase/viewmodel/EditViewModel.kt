0035bf0db8e95767ddae7e9255d0e03d86f7b02d
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-04 14:12:38 +0700

    menambah fungsi validasi input untuk memastikan semua field terisi

--- app/src/main/java/com/example/a074_roomdatabase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/a074_roomdatabase/viewmodel/EditViewModel.kt
@@ -27,4 +27,12 @@ class EditViewModel (
             isEntryValid = validasiInput(detailSiswa)
         )
     }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() &&
+                    alamat.isNotBlank() &&
+                    telpon.isNotBlank()
+        }
+    }
 }
